c3fe3eda2c6f6e551d541481be59ab34d1eb0799
siamcodes <siamcodes@gmail.com>
2021-07-11 23:03:16 +0700

    initila

--- models/blog.js
+++ models/blog.js
@@ -19,7 +19,7 @@ const blogSchema = new mongoose.Schema(
             type: {},
             required: true,
             min: 200,
-            max: 20000000
+            max: 5000000
         },
         excerpt: {
             type: String,
